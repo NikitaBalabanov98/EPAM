0e7fc0d476429880a15acc408232b1fed02cf7a1
NikitaBalabanov98 <balin98.16@gmail.com>
2020-01-28 00:01:53 +0300

    RAM

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,24 +10,59 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int x = 1;
+            Func1(x);
+            Console.WriteLine("Call Func1. x = " + x);
+
+            x = 1;
+            Func2(x);
+            Console.WriteLine("Call Func2. x = " + x);
+
+            x = 1;
+            Func3(x);
+            Console.WriteLine("Call Func3. x = " + x);
 
 
             Point p1 = new Point(1, 3, '*');
-
-            p1.Draw();
+            Move(p1, 10, 10);
+            Console.WriteLine("Call Move.p1.x = " + p1.x + ", p1.y = " + p1.y);
 
             Point p2 = new Point(4, 5, '#');
+            p1 = p2;
+            p2.x = 8;
+            p2.y = 8;
+            Console.WriteLine("p1=p2. p1.x = " + p1.x + ", p1.y = " + p1.y + ", p2.x = " + p2.x + ", p2.y = " + p2.y);
 
-            p2.Draw();
-
+            
 
             Console.ReadLine();
         }
-
-        static void Draw(int x, int y, char sym)
+        static void Func1(int value)
         {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
+
+
+        }
+        static void Func2(int value)
+        {
+            value = value + 1;
+
+        }
+
+        static void Func3(int X)
+        {
+            X = X + 1;
+        }
+        static void Move(Point p, int dx, int dy)
+        {
+            //p.x = p.x + dx;
+            //p.y = p.y + dy;
+            p.x += dx;
+            p.y += dy;
+        }
+
+        static void Reset(Point p)
+        {
+            p = new Point();
         }
     }
 }
